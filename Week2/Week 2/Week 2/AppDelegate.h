cabfc05dd55a37169d8eb2368f9b33bcc1dc7ed5
Nick Weil <speedquest@fullsail.edu>
2012-09-04 23:04:14 -0400

    Inital project commit...

---
+++ Week2/Week 2/Week 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week 2
+//
+//  Created by Nicholas Weil on 9/4/12.
+//  Copyright (c) 2012 Nicholas Weil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Week2/Week 2/Week 2/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Week 2
+//
+//  Created by Nicholas Weil on 9/4/12.
+//  Copyright (c) 2012 Nicholas Weil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
